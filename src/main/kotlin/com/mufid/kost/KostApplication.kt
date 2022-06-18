32e1ed97fb335209e9df98d94f521c2387a030d3
imufiid <imufiid98@gmail.com>
2022-06-18 17:11:20 +0700

    first commit

---
+++ src/main/kotlin/com/mufid/kost/KostApplication.kt
@@ -0,0 +1,11 @@
+package com.mufid.kost
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KostApplication
+
+fun main(args: Array<String>) {
+	runApplication<KostApplication>(*args)
+}
---
+++ src/test/kotlin/com/mufid/kost/KostApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mufid.kost
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KostApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
